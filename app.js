7bd09bd32dc12bb343112d2db4306940018d1d46
IcaroNascimento <104856582+icaronascimento-git@users.noreply.github.com>
2024-01-25 19:37:03 -0300

    commit-inicial

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('P');
+paragrafo.innerHTML = 'Digite um numero entre 1 e 30';
+
+function verificarChute() {
+    console.log('Foi clicado')
+}
